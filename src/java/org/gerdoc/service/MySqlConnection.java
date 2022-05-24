9c059a438354a300e2d31b2cb873cdadb6067da9
gerdoc <ggerdoc@gmail.com>
2022-05-24 14:44:04 -0500

    Se agrega la lista y la alta

--- src/java/org/gerdoc/service/MySqlConnection.java
+++ src/java/org/gerdoc/service/MySqlConnection.java
@@ -8,6 +8,7 @@ package org.gerdoc.service;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -181,6 +182,47 @@ public class MySqlConnection implements Serializable
             ex.printStackTrace();
         }
         return null;
+    }
+    
+    public static boolean addDos( Dos dos )
+    {
+        Connection connection = null;        
+        String sql = null;
+        PreparedStatement preparedStatement = null;
+        int row = 0;
+        try 
+        {
+            if( dos == null || dos.getCampo1() == null || dos.getCampo2() == null )
+            {
+                return false;
+            }
+            connection = getConnection( );
+            if( connection == null )
+            {
+                return false;
+            }
+            sql = "insert into tbl_dos(campo1,campo2) values( ? , ? )";
+            preparedStatement = connection.prepareStatement(sql);
+            if( preparedStatement == null )
+            {
+                return false;
+            }
+            preparedStatement.setInt(1, dos.getCampo1() );
+            preparedStatement.setString(2, dos.getCampo2());
+            row = preparedStatement.executeUpdate();
+            if( row == 0 )
+            {
+                return false;
+            }
+            preparedStatement.close();
+            closeConnection(connection);
+            return true;
+        } 
+        catch (SQLException ex) 
+        {
+            ex.printStackTrace();
+        }
+        return false;
     }
     
 }
