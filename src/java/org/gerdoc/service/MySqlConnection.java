8f31be096c9307e213da01a7c75d74555a5a0b73
gerdoc <ggerdoc@gmail.com>
2022-05-26 14:46:37 -0500

    ABC de tbl_dos

--- src/java/org/gerdoc/service/MySqlConnection.java
+++ src/java/org/gerdoc/service/MySqlConnection.java
@@ -225,4 +225,131 @@ public class MySqlConnection implements Serializable
         return false;
     }
     
+    public static Dos getDosById( Integer id )
+    {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Dos dos = null;
+        String sql = "SELECT * FROM TBL_DOS where id = ?";
+        
+        try 
+        {
+            connection = getConnection( );
+            if( connection == null )
+            {
+                return null;
+            }
+            preparedStatement = connection.prepareStatement(sql);
+            if( preparedStatement == null )
+            {
+                return null;
+            }
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            if( resultSet == null )
+            {
+                return null;
+            }
+            while( resultSet.next() )
+            {
+                dos = new Dos();
+                dos.setId( resultSet.getInt(1) );
+                dos.setCampo1(resultSet.getInt(2) );
+                dos.setCampo2(resultSet.getString(3) );
+            }
+            preparedStatement.close();
+            resultSet.close();
+            closeConnection(connection);
+            return dos;
+        } 
+        catch (SQLException ex) 
+        {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static boolean updateDos( Dos dos )
+    {
+        Connection connection = null;        
+        String sql = null;
+        PreparedStatement preparedStatement = null;
+        int row = 0;
+        try 
+        {
+            if( dos == null || dos.getCampo1() == null || dos.getCampo2() == null || dos.getId() == null )
+            {
+                return false;
+            }
+            connection = getConnection( );
+            if( connection == null )
+            {
+                return false;
+            }
+            sql = "update tbl_dos set campo1= ?,campo2=? where id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            if( preparedStatement == null )
+            {
+                return false;
+            }
+            preparedStatement.setInt(1, dos.getCampo1() );
+            preparedStatement.setString(2, dos.getCampo2());
+            preparedStatement.setInt(3, dos.getId());
+            row = preparedStatement.executeUpdate();
+            if( row == 0 )
+            {
+                return false;
+            }
+            preparedStatement.close();
+            closeConnection(connection);
+            return true;
+        } 
+        catch (SQLException ex) 
+        {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+    
+    public static boolean deleteDos( Integer id )
+    {
+        Connection connection = null;        
+        String sql = null;
+        PreparedStatement preparedStatement = null;
+        int row = 0;
+        try 
+        {
+            if( id == null || id == 0 )
+            {
+                return false;
+            }
+            connection = getConnection( );
+            if( connection == null )
+            {
+                return false;
+            }
+            sql = "delete from tbl_dos where id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            if( preparedStatement == null )
+            {
+                return false;
+            }
+            preparedStatement.setInt(1, id);
+            row = preparedStatement.executeUpdate();
+            if( row == 0 )
+            {
+                return false;
+            }
+            preparedStatement.close();
+            closeConnection(connection);
+            return true;
+        } 
+        catch (SQLException ex) 
+        {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+    
 }
